ac2bce8632a8a8f961d416b81a586ed015ecf55b
satyam <satyam@navigolabs.com>
2026-05-13 12:11:14 +0530

    feat: get single task

--- node-auth/src/controllers/taskController.js
+++ node-auth/src/controllers/taskController.js
@@ -29,4 +29,28 @@ const getTasks = async (req, res) => {
   }
 };
 
-module.exports = { createTask, getTasks };
+const getTaskById = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+
+    if (!task) {
+      return res.status(404).json({
+        message: "Task not found",
+      });
+    }
+
+    if (task.user.toString() !== req.user) {
+      return res.status(401).json({
+        message: "Not authorized",
+      });
+    }
+
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+module.exports = { createTask, getTasks, getTaskById };
--- node-auth/src/routes/taskRoutes.js
+++ node-auth/src/routes/taskRoutes.js
@@ -2,9 +2,14 @@ const express = require("express");
 const router = express.Router();
 const protect = require("../middleware/authMiddleware");
 
-const { createTask, getTasks } = require("../controllers/taskController");
+const {
+  createTask,
+  getTasks,
+  getTaskById,
+} = require("../controllers/taskController");
 
 router.post("/createTask", protect, createTask);
 router.get("/getTasks", protect, getTasks);
+router.post("/:id", protect, getTaskById);
 
 module.exports = router;
